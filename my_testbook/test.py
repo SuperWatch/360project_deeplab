42f4b78c4aeb869985d0eaf391286c6bffc9e18b
susuGit <suyipei@qq.com>
2018-05-23 16:37:12 +0800

    2018-0523

--- my_testbook/test.py
+++ my_testbook/test.py
@@ -22,13 +22,33 @@ def test_tile():
     # [1 2 1 2 1 2]
     # [3 4 3 4 3 4]
     # [5 6 5 6 5 6]]
+def tset_print_all_ops(pb_file):
+    """
+    , img_file
+    Predict and visualize by TensorFlow.
+    :param pb_file:
+    :param img_file:
+    :return:
+    """
+    with tf.gfile.GFile(pb_file, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
 
-def main():
+    with tf.Graph().as_default() as graph:
+        # tf.import_graph_def(graph_def, name="prefix")
+        tf.import_graph_def(graph_def)
+
+    for op in graph.get_operations():
+        print(op.name)
+
+def test_argmax_dtype():
     test = np.array([[1, 2, 3], [2, 3, 4], [5, 4, 3], [8, 7, 2]])
     out1=np.argmax(test, 0)#输出：[3, 3, 1]
     out2=np.argmax(test, 1)#输出：[2, 2, 0, 0]
     print(out1.dtype)
 
 
+
+
 if __name__ == '__main__':
-    main()
+    tset_print_all_ops("F:\\Modellib\\deeplabv3_mnv2_pascal_train_aug\\frozen_inference_graph.pb")
